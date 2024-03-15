3ebbc7ee92e8e60ff2b2e25ee49c9b537179f482
Anthony Destenay <anthony.destenay@gmail.com>
2024-03-15 13:23:03 +0100

    Added legal notice

--- components/AppFooter.vue
+++ components/AppFooter.vue
@@ -1,6 +1,11 @@
 <template>
   <div class="opacity-50 text-sm p-4 text-center">
     Tous les logos, marques et images de ce site sont la propriété de Keen
-    Games. Ce site n'est pas affilié avec l'éditeur. Réalisé par Thoanny
+    Games. Ce site n'est pas affilié avec l'éditeur. Réalisé par
+    <a href="https://thoanny.fr" target="_blank" class="underline">Thoanny</a>
+    &bull;
+    <NuxtLink :to="{ name: 'legal-notice' }" class="underline"
+      >Mentions légales</NuxtLink
+    >
   </div>
 </template>
