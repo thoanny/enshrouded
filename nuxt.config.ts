b5cc967f97d2251a933ce0460f7796018f369038
Anthony Destenay <anthony.destenay@gmail.com>
2024-03-20 08:52:52 +0100

    Added sitemap

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -8,5 +8,11 @@ export default defineNuxtConfig({
       autoprefixer: {},
     },
   },
-  modules: ["@nuxtjs/mdc"],
+  site: {
+    url: "https://enshrouded.vercel.app",
+  },
+  sitemap: {
+    sources: ["http://127.0.0.1:8000/api/enshrouded/sitemap"],
+  },
+  modules: ["@nuxtjs/mdc", "@nuxtjs/sitemap"],
 });
